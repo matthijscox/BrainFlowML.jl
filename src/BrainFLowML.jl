5f43924785a7386b328e0176181ea2c4e1476e41
matthijscox <matthijs.cox@gmail.com>
2021-01-13 21:24:07 +0100

    start of data loading

--- src/BrainFLowML.jl
+++ src/BrainFLowML.jl
@@ -1,5 +1,20 @@
 module BrainFlowML
 
+    using CSV
+    using DataFrames
+
     const testdata_path = abspath(joinpath(@__DIR__, "../test/testdata"))
+
+    struct BioData
+        raw
+        emg_channels::AbstractVector{Int} # indices of the channel columns
+    end
+
+    # make each_channel(::BioData) iterator 
+
+    function load_gesture(filepath)
+        df = DataFrame(CSV.File(filepath))
+        return BioData(df, 1:8)
+    end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using Test
+using DataFrames
 using BrainFlowML
 
+function get_gesture(file_name)
+    test_filepath = joinpath(BrainFlowML.testdata_path, file_name)
+    return BrainFlowML.load_gesture(test_filepath)
+end
+
 @testset "BrainFlowML" begin
-    @test 1==2
+    @test get_gesture("left_gesture.csv") isa BrainFlowML.BioData
 end
