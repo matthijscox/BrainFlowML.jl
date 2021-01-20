f213a004d007d8960683661a2642714510557bd8
matcox <matthijs.cox@asml.com>
2021-01-20 16:59:46 +0100

    makie prediction

--- test/realtime_predict.jl
+++ test/realtime_predict.jl
@@ -3,6 +3,7 @@ using BrainFlow
 using BrainFlowML
 using JLD2
 using DecisionTree # maybe MLJ in future
+using Makie
 
 # load a stored model
 test_model = joinpath(BrainFlowML.testdata_path, "model_file.jld2")
@@ -19,14 +20,24 @@ BrainFlow.start_stream(board_shim)
 sleep(0.5) # wait a moment for brainflow buffer to fill, and other startup effects
 
 # make a prediction with the model:
-data = BrainFlow.get_current_board_data(modeled_sample_size, board_shim)
-v = BrainFlowML.preprocess_brainflow_data(A', BrainFlow.get_emg_channels(board_id))
-predicted_gesture = apply_forest(model, v')[1]
+function predict_gesture_on_data(board_shim::BrainFlow.BoardShim, modeled_sample_size::Int = 128)
+    data = BrainFlow.get_current_board_data(modeled_sample_size, board_shim)
+    v = BrainFlowML.preprocess_brainflow_data(data, BrainFlow.get_emg_channels(board_shim.board_id))
+    predicted_gesture = apply_forest(model, v')[1]
+end
 
-# visualize the gesture somehow? Just text in Makie?
-# using Makie
-# s = Scene()
-# text("bla", show_axis=false)
+# initialize a text object in Makie
+gesture_map = ["", "left", "right", "fist", "spread"]
+scene = Scene()
+text_holder = Node("start")
+text(text_holder, show_axis=false)
+display(scene)
+
+# update continuously
+while True
+    sleep(0.02)
+    text_holder[] = gesture_map(predict_gesture_on_data(board_shim, modeled_sample_size))
+end
 
 # done?
 # BrainFlow.stop_stream(board_shim)
